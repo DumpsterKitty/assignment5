f1c9b92824aba0ce784a2795eecf7b3ab11646d8
DumpsterKitty <116283475+DumpsterKitty@users.noreply.github.com>
2022-12-02 11:23:49 -0500

    test js

--- js/script.js
+++ js/script.js
@@ -83,9 +83,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [buildAndShowHomeHTML], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, 
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
+
 // *** finish **
 
 
@@ -114,15 +115,15 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+      //
 
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "short_name", "' " + chosenCategoryShortName + "' ");
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
-
+      // ....
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
@@ -152,7 +153,7 @@ dc.loadMenuCategories = function () {
 dc.loadMenuItems = function (categoryShort) {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
-    menuItemsUrl + categoryShort + ".json",
+    menuItemsUrl + categoryShort + '.json',
     buildAndShowMenuItemsHTML);
 };
 
